46125d612beca82000dc9929dec578b058e7f765
Anupam Datta <anupamdatta10@gmail.com>
2026-07-05 22:28:19 +0530

    fix login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,13 +8,12 @@ const router = express.Router();
 
 router.post(['/register', '/newuser'], userController.registerUser);
 router.post(['/login', '/olduser'], userController.loginUser);
-// router.post(['/forgot-password', '/sendmail'], userController.sendForgotPasswordOtp);
-// router.post(['/verify-otp', '/cpass'], userController.verifyOtpAndResetPassword);
 router.get(['/button-status', '/getbuttonstatus'], decodeJwtToken, userController.getButtonStatusByUser);
 router.put(['/button-status/:buttonId'], decodeJwtToken, userController.updateButtonStatusByUser);
 router.post('/wifi-data', decodeJwtToken, wifiController.storeWifiData);
 router.get('/wifi-data', decodeJwtToken, wifiController.listWifiData);
 router.post('/sendmail', userController.sendForgotPasswordOtp);
+router.post('/verify-otp', userController.verifyOtp);
 router.post('/cpass', userController.verifyOtpAndResetPassword);
 
 module.exports = router;
